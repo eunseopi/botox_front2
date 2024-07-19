4ce380fc9ff7fa2782574f5afbd41497ffa3aaa8
Baek Dongryeol <think0507@naver.com>
2024-07-19 20:47:36 +0900

    메소드 post content-type = application/json
    access token 받는 로직 수정

--- src/pages/loginpage/LoginPage.js
+++ src/pages/loginpage/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {useNavigate} from "react-router-dom";
-import BotoxImage from '../../images/white.png'
+import { useNavigate } from "react-router-dom";
+import BotoxImage from '../../images/white.png';
 import menuImage from "../../images/menu.png";
 
 const LoginPage = () => {
@@ -12,7 +12,10 @@ const LoginPage = () => {
         try {
             const response = await fetch('https://botox-chat.site/api/users/login', {
                 method: 'POST',
-                body: new URLSearchParams({
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
                     username: email,
                     password: password,
                 }),
@@ -24,9 +27,10 @@ const LoginPage = () => {
                 const data = await response.json();
                 console.log('Response data:', data);
 
-                if (data.accessToken) {
-                    localStorage.setItem('token', data.accessToken);
-                    console.log('Token saved to localStorage:', data.accessToken);
+                // 여기서 accessToken을 올바르게 추출합니다.
+                if (data.data && data.data.accessToken) {
+                    localStorage.setItem('token', data.data.accessToken);
+                    console.log('Token saved to localStorage:', data.data.accessToken);
                     navigate('/main');
                 } else {
                     console.log('로그인에 실패했습니다. 토큰이 제공되지 않았습니다.');
@@ -52,23 +56,20 @@ const LoginPage = () => {
         }
     };
 
-
-
     const handleSignUp = () => {
         navigate('/signup');
     }
-
 
     return (
         <div className="bg-customMainBg flex flex-col items-center justify-center">
             <div className="w-full bg-customTopNav h-10 mb-60">
                 <nav className="flex items-center px-4">
-                    <img src={menuImage} alt="Menu" className="w-10 h-10 p-2 mr-2"/>
+                    <img src={menuImage} alt="Menu" className="w-10 h-10 p-2 mr-2" />
                 </nav>
             </div>
             <div className="bg-customLoginBg p-8 rounded-lg shadow-2xl w-1/3 h-1/2">
                 <h1 className="text-3xl font-bold mb-6 flex items-center justify-center">
-                    <img src={BotoxImage} alt="Botox" className="w-8 h-8 mr-3"/>
+                    <img src={BotoxImage} alt="Botox" className="w-8 h-8 mr-3" />
                     B o t o x
                 </h1>
                 <h3 className="text-customDarkBlue text-sm mb-2">이메일 또는 아이디</h3>
@@ -76,7 +77,7 @@ const LoginPage = () => {
                     type="email"
                     id="email"
                     value={email}
-                    onChange={(e) => {setEmail(e.target.value)}}
+                    onChange={(e) => { setEmail(e.target.value) }}
                     className="w-full p-2 mb-4 border-0 rounded"
                 />
                 <h3 className="text-customDarkBlue text-sm mb-2">비밀번호</h3>
@@ -84,7 +85,7 @@ const LoginPage = () => {
                     type="password"
                     id="password"
                     value={password}
-                    onChange={(e) => {setPassword(e.target.value)}}
+                    onChange={(e) => { setPassword(e.target.value) }}
                     className="w-full p-2 mb-6 border-0 rounded"
                 />
                 <button
